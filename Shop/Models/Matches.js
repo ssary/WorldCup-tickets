f5e21b956727bd0eab7f3829a528b4f2c800c5b0
Omar_ElBarbary <omar.m.elbarbary@gmail.com>
2022-12-15 21:55:45 +0200

    changed matches schema to fit the new master list

--- Shop/Models/Matches.js
+++ Shop/Models/Matches.js
@@ -3,51 +3,57 @@ const { Schema } = mongoose;
 
 
 const matchSchema = new Schema({
-      MatchNumber: {type: Number
-        ,required: true,
-        unique: true
-      },
-      RoundNumber: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 7
-      },
-      DateUtc: {
-        type: Date,
-        required: true
-      },
-      Location: {
-        type: String,
-        required: true
-      },
-      StadiumCapacity: {
-        type: Number,
-        required: true,
-        min: 0
-      },
-      HomeTeam: {
-        type: String,
-      },
-      AwayTeam: {
-       type:String
-      },
-      HomeTeamScore: {
-        type: Number,
-        min: 0
-      },
-      AwayTeamScore: {
-        type: Number,
-        min: 0
-      },
-      Tickets: {
-        type: Number,
-        min: 0,
-        default : function() { 
-          return this.StadiumCapacity;
-      }
-      }
+  MatchNumber: {
+    type: Number
+    , required: true,
+    unique: true
+  },
+  RoundNumber: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 7
+  },
+  DateUtc: {
+    type: Date,
+    required: true
+  },
+  Location: {
+    type: String,
+    required: true
+  },
+  HomeTeam: {
+    type: String,
+  },
+  AwayTeam: {
+    type: String
+  },
+  HomeTeamScore: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  AwayTeamScore: {
+    type: Number,
+    min: 0,
+    default: 0
+  },
+  availability: {
+    category1: {
+      count: { type: Number },
+      price: { type: Number, default: 75 }
+    },
+    category2: {
+      count: { type: Number },
+      price: { type: Number, default: 125 }
+    }, category3: {
+      count: { type: Number },
+      price: { type: Number, default: 195 }
     }
-  );
-const Matches = mongoose.model('Matches',matchSchema,'Shop');
+
+  }
+}
+
+);
+const Matches = mongoose.model('Matches', matchSchema, 'Shop');
 export default Matches;
