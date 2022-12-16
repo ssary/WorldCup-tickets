78113cd8bb99108ecbd7e7ad23732824734380b0
Omar_ElBarbary <omar.m.elbarbary@gmail.com>
2022-12-16 17:37:34 +0200

    changed routes to avoid conflict

--- Shop/routes/matches.js
+++ Shop/routes/matches.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import{getMatches,getMatch,addMatches,updateMatch, ReserveMatchTickets,HoldMatchTickets} from "../controller/matches.js"
+import{getMatches,getMatch,addMatches,updateMatch, ReserveMatchTickets,HoldMatchTickets, UpdateMatchTickets} from "../controller/matches.js"
 
 const router = express.Router();
 
 router.get('/',getMatches)
 router.get('/:MatchNumber',getMatch)
 router.post('/',addMatches)
-router.put('/:MatchNumber',updateMatch)
+router.put('/update/:MatchNumber',updateMatch)
 router.put('/Reserve',ReserveMatchTickets)
 router.put('/Pending',HoldMatchTickets)
-
+router.patch('/',UpdateMatchTickets)
 export default router;
