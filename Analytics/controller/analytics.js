c5460bb6f179bad3234325c2bb36d05d67a385aa
sary nasser <53094371+ssary@users.noreply.github.com>
2023-01-01 19:47:41 +0200

    analytics backend - countEachCategory: count how many tickets reserved for each category - countMatchesTickets: count how many tickets reserved for each match - statePercentage: percentage of each state(cancelled, reserved, pending)

--- Analytics/controller/analytics.js
+++ Analytics/controller/analytics.js
@@ -1,6 +1,8 @@
+import { range } from 'balanced-match'
+
 const messageModel = require('../model/message.js')
 
-const postMessage = async (req, res) =>{
+export const postMessage = async (req, res) =>{
     const {matchNumber, category, price, state, quantity} = req.body
     try {
         const newMessage = new messageModel({'category':category, 'state': state, 'price': price,
@@ -13,3 +15,67 @@ const postMessage = async (req, res) =>{
     }
 }
 
+export const countEachCategory = async (req,res)=>{
+    try {
+        const messages = await messageModel.find({state: 'TICKET_RESERVED'})
+        let category1 = 0, category2=0, category3=0
+        messages.map((message)=>{
+            if(message.state === 1){
+                category1 += message.quantity
+            }
+            else if(message.state === 2){
+                category2 += message.quantity
+            }
+            else{
+                category3 += message.quantity
+            }
+        })
+        res.status(200).send({'category1': category1, 'category2': category2, 'category3': category3})
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+export const countMatchesTickets = async (req,res)=>{
+    try {
+        let matchFreq = new Array(65).fill(0)
+        //let roundFreq = newArray(8).fill(0)
+        
+        const matches = await messageModel.find({state: 'TICKET_RESERVED'})
+        matches.map((match)=>{
+            const matchNumber = match.MatchNumber
+            //const round = match.round
+            matchFreq[matchNumber] += match.quantity
+        })
+        let matchReservedTickets = {}
+        for(let i=0;i<65;i++){
+            matchReservedTickets[i] = matchFreq[i]
+        }
+        matchReservedTickets.sort((a,b)=>b-a)
+        res.status(200).send(matchReservedTickets.slice(0,10))
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+
+export const statePercentage = async (req, res)=>{
+    try {
+        const messages = await messageModel.find()
+        let countReserved=0, countCancelled=0, countPending=0
+        messages.map((message)=>{
+            if(message.state === 'TICKET_RESERVED'){
+                countReserved += message.quantity
+            }
+            else if(message.state === 'TICKET_PENDING'){
+                countPending += message.quantity
+            }
+            else{
+                countCancelled += message.quantity
+            }
+        })
+        res.status(200).send({'countReserved': countReserved, 'countPending': countPending, 'countCancelled': countCancelled}) 
+    } catch (error) {
+        res.send(error.message)
+    }
+}
--- Analytics/routes/analytics.js
+++ Analytics/routes/analytics.js
@@ -1,8 +1,10 @@
 const express = require ('express')
-const {postMessage} = require('../controller/analytics.js')
+const {postMessage, countEachCategory, countMatchesTickets} = require('../controller/analytics.js')
 
 const router = express.Router();
 
 router.post('/', postMessage);
+router.get('/countCategories', countEachCategory)
+router.get('/countMatches', countMatchesTickets)
 
 module.exports = router;
