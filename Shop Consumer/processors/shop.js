60c2b4fce28b3579b969b5f9f24a5d1723ca4cb1
Omar_ElBarbary <omar.m.elbarbary@gmail.com>
2023-01-04 15:08:58 +0200

    change localhost to deployed site

--- Shop Consumer/processors/shop.js
+++ Shop Consumer/processors/shop.js
@@ -2,21 +2,21 @@ const axios = require('axios');
 
 const processPendingTicket = async (message) => {
   console.log('[processPendingTicket]',message)
- //await axios.patch("http://localhost:4000/api/matches/",{"MatchNumber":matchNumber,"category":tickets.category,"quantity":tickets.quantity,"action":action})
+ await axios.patch("https://world-cup-shop-microservice.vercel.app/api/matches",{"matchNumber":message.body.matchNumber,"category":message.body.tickets.category,"quantity":message.body.tickets.quantity,"action":message.meta.action});
  //commented out because people keep sending messages
  //local host will change to vercel after deployment
   return Promise.resolve('[processPendingTicket]')
 };
 
 const processReservedTicket = async (message) => {
- //await axios.patch("http://localhost:4000/api/matches/",{"MatchNumber":message.body.matchNumber,"category":message.body.tickets.category,"quantity":message.body.tickets.quantity,"action":message.meta.action})
+  await axios.patch("https://world-cup-shop-microservice.vercel.app/api/matches",{"matchNumber":message.body.matchNumber,"category":message.body.tickets.category,"quantity":message.body.tickets.quantity,"action":message.meta.action});
  // commented out because people keep sending messages
  //local host will change to vercel after deployment
   console.log('[processReservedTicket]', message)
   return Promise.resolve('[processReservedTicket]')
 };
 const processCancelledTicket = async (message) => {
-  //await axios.patch("http://localhost:4000/api/matches/",{"MatchNumber":message.body.matchNumber,"category":message.body.tickets.category,"quantity":message.body.tickets.quantity,"action":message.meta.action})
+  await axios.patch("https://world-cup-shop-microservice.vercel.app/api/matches",{"matchNumber":message.body.matchNumber,"category":message.body.tickets.category,"quantity":message.body.tickets.quantity,"action":message.meta.action});
   // commented out because people keep sending messages
   //local host will change to vercel after deployment
    console.log('[processCancelledTicket]', message)
@@ -24,7 +24,15 @@ const processCancelledTicket = async (message) => {
  };
  
  const processMasterlist = async (message) => {
-   //await axios.patch("http://localhost:4000/api/matches/",{"MatchNumber":message.body.matchNumber,"category":message.body.tickets.category,"quantity":message.body.tickets.quantity,"action":message.meta.action})
+
+   await axios.post("https://world-cup-shop-microservice.vercel.app/api/matches/",{"matchNumber":message.matchNumber ,
+    "roundNumber": message.roundNumber,
+    "location": message.location,
+    "dateUtc":message.dateUtc,
+    "availability": message.availability,
+    "homeTeam": message.homeTeam,
+    "awayTeam": message.awayTeam
+})
   console.log('[processMasterlist]', message)
   return Promise.resolve('[processMasterlist]')
 };
